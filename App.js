67ace27bc85e70b59ab562a8f3611e67f5276ee4
Grant Flesher <gflesher@student12.cse.nd.edu>
2024-06-30 15:40:45 -0400

    Added Parse Initialization

--- App.js
+++ App.js
@@ -3,6 +3,12 @@ import {
   render,
 } from "https://unpkg.com/htm/preact/standalone.module.js";
 import Main from "./Components/Main/Main.js";
+import { Env } from "./Components/environments.js";
+//parse is imported  in index.html
+
+Parse.initialize(Env.APPLICATION_ID, Env.JAVASCRIPT_KEY);
+Parse.serverURL = Env.SERVER_URL;
+
 
 function App() {
   return html`<${Main} />`;
