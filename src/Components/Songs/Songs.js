c361cf3b77aa519da1246222ed8a2b425886355c
Grant Flesher <gflesher@student12.cse.nd.edu>
2024-07-22 17:25:13 -0400

    Added genres to songs.js page

--- src/Components/Songs/Songs.js
+++ src/Components/Songs/Songs.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { getGenreById } from '../../Services/genreModel';
 
 const Songs = ({ songs = [] }) => {
+  const [genres, setGenres] = useState({});
+
+  useEffect(() => {
+
+    //gets the genres of each song
+    const fetchGenres = async () => {
+      const genresMap = {};
+
+      //Iterate over each song to get each songs genre 
+      for (const song of songs) {
+        const genrePointer = song.get('Genre');
+
+        //gets the genre if it hasn't been fetched yet
+        if (genrePointer && !genresMap[genrePointer.id]) {
+          const genreData = await getGenreById(genrePointer.id);
+          genresMap[genrePointer.id] = genreData.get('Name');
+        }
+      }
+
+      setGenres(genresMap);//sets genre to each song
+    };
+
+    //fetchs genres if songs are in the list
+    if (songs.length > 0) {
+      fetchGenres();
+    }
+  }, [songs]);
   return (
     <div>
       <h1>Songs Page</h1>
@@ -9,11 +37,15 @@ const Songs = ({ songs = [] }) => {
       <p>Song List:</p>
       <ul>
         {songs.length > 0 ? (
-          songs.map((song) => (
-            <li key={song.id}>
-              <Link to={`/song/${song.id}`}>{song.get('Title')} by {song.get('Artist')}</Link>
-            </li>
-          ))
+          songs.map((song) => {
+            const genrePointer = song.get('Genre');
+            const genreName = genrePointer ? genres[genrePointer.id] : 'Unknown';
+            return (
+              <li key={song.id}>
+                <Link to={`/song/${song.id}`}>{song.get('Title')}</Link> by {song.get('Artist')} - {genreName}
+              </li>
+            );
+          })
         ) : (
           <p>No songs available.</p>
         )}
